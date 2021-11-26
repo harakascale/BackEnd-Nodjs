649667a0fd40e7a37f34329eccb822319decddd6
Greg <harakascale@gmail.com>
2021-11-26 15:25:57 -0500

    second commit

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Access-Control-Allow-Headers, X-Requested-With, Accept, Authorization"
   );
   res.setHeader(
    "Access-Control-Allow-Methods",
